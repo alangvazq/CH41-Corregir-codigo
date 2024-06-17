d2228457a361ae21c61cdcda7a17b60a2dc93a5d
alangvazq <alan.g.vazq@gmail.com>
2024-06-17 12:22:38 -0600

    Correción de errores principales

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,21 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+//! Se agrego un . por ser clases
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+//! Se agrego async para el await
+async function displayUser(username) {
+    $n.textContent = 'cargando...';
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    //! Se convirtio a json la respuesta
+    const data = await response.json();
+    console.log(data);
+    //! Se cambio cambio '' por ``
+    $n.textContent = `${data.name}`;
+    $b.textContent = `${data.blog}`;
+    $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
